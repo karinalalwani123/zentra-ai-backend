3a41d96c13f1016afa550c50c1d1405100f19058
Karina lalwani <kareenalalwani123@gmail.com>
2026-05-20 19:28:37 +0530

    save local changes

--- email-ui/src/firebase.js
+++ email-ui/src/firebase.js
@@ -7,13 +7,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDR8BRFRz587lhNLA2NBH8kdJrJI5v5GII",
-  authDomain: "emailassistant-d737b.firebaseapp.com",
-  projectId: "emailassistant-d737b",
-  storageBucket: "emailassistant-d737b.firebasestorage.app",
-  messagingSenderId: "332757522958",
-  appId: "1:332757522958:web:f7cb9ad371dee5508d7a5f",
-  measurementId: "G-0KQ864MY6Q"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 
